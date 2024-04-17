49042bb4da44653461955c19eb117c17362ea4cf
Sigourn <ldelfino92@gmail.com>
2024-04-17 20:47:12 -0300

    Primera version

---
+++ TrabajoPractico/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoPractico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cboMarcas.Items.Add("Apple");
+            cboMarcas.Items.Add("Huawei");
+            cboMarcas.Items.Add("Motorola");
+            cboMarcas.Items.Add("Samsung");
+            cboMarcas.Items.Add("Sony");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
