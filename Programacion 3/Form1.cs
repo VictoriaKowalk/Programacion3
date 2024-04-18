759539e83476265c09309542d29cc05195e3104f
Lucas <50838647+Sigourn@users.noreply.github.com>
2024-04-17 21:10:22 -0300

    Primer diseño de ventana

--- Programacion 3/Form1.cs
+++ Programacion 3/Form1.cs
@@ -8,13 +8,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Programacion_3
+namespace TrabajoPractico
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            cboMarcas.Items.Add("Apple");
+            cboMarcas.Items.Add("Huawei");
+            cboMarcas.Items.Add("Motorola");
+            cboMarcas.Items.Add("Samsung");
+            cboMarcas.Items.Add("Sony");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
