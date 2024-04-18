16c3f8b7e4cdcf045c0e872fc7792f54c8833897
Mathias Ahumada <125514738+mathias-ahumada@users.noreply.github.com>
2024-04-18 14:39:16 -0300

    Update en program
    agregue un using

--- Programacion 3/Program.cs
+++ Programacion 3/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using TrabajoPractico;
 namespace Programacion_3
 {
     internal static class Program
